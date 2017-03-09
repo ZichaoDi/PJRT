99988ed6413b8f12c533fc043730d26b1c39e79a
Matthew Otten <mjo98@cornell.edu>
2017-03-09 10:24:43 -0800

    Added prototype TAO calls.

--- pjrt.c
+++ pjrt.c
@@ -1,19 +1,26 @@
 
 #include <math.h>
 #include <stdlib.h>
+//#include "petsc.h"
 
 int main(int argc,char **args){
   int i,j,n_tau,n_theta,n_e,n_E,n_x,n_y,n_xy,e,E,v;
-  double L_ijv;
-  doulbe omega[4]={-2,2,-2,2};
+  int n_total_1,n_total_2,my_start,my_end,i_p,j_p;
+  PetscInt ierr;
+  double A_ijveE;
+  double omega[4]={-2,2,-2,2};
+  double primary_source[2],primary_detector[2],source[2],detector[2];
+  double *theta;
   double xbox[4]={omega[0],omega[0],omega[1],omega[1],omega[0]};
   double ybox[4]={omega[2],omega[3],omega[3],omega[2],omega[2]};
+
+
 
   Mat A, x, MU_e;
 
   n_x=100;
   n_y=100;
-  n_tau = floor(sqrt(n_x^2+n_y^2));
+  n_tau = floor(sqrt(n_x*n_xy+n_y*n_y));
   n_theta = 73;
   n_xy = n_x * n_y;
   n_E = 2000;
@@ -57,7 +64,7 @@ int main(int argc,char **args){
       I_ij = 0;
       for(v=0;v<n_subv;v++){
         for (e=0;e<n_e;e++){
-      I_ij += x[sub_v[v],e]*Lvec[v]*MU_e[e,1];
+          I_ij += x[sub_v[v],e]*Lvec[v]*MU_e[e,1];
           // O_ijve = calculate_O(); not self-absorption for now; 
 
           for (E=0;E<n_E;E++){
@@ -65,7 +72,7 @@ int main(int argc,char **args){
 
             A_ijveE    = Lvec[v]*I_ij*M_eE;
 
-            i_p        = [i-1]*n_theta+j+(E-1)*(n_theta*n_tau);
+            i_p        = (i-1)*n_theta+j+(E-1)*(n_theta*n_tau);
             j_p        = (sub_v[v]-1)*n_e+e;
             //A[i_p,j_p] = A_ijveE;
             if (A_ijveE!=0){
@@ -78,7 +85,31 @@ int main(int argc,char **args){
     }
   }
 
-  TAO_Solve(A,x,b);
+  //Setup TAO solver
+
+  /* Create TAO solver with initial solution method */
+  ierr = TaoCreate(PETSC_COMM_WORLD,&tao); CHKERRQ(ierr);
+  ierr = TaoSetType(tao,TAOLMVM); CHKERRQ(ierr);
+
+  /* Set solution vec and an initial guess */
+  ierr = VecSet(x,0); CHKERRQ(ierr);
+  ierr = TaoSetInitialVector(tao,x); CHKERRQ(ierr);
+
+  /* Set routines for function, gradient, etc */
+  ierr = TaoSetObjectiveAndGradientRoutine(tao,FormFunctionGradient,NULL); CHKERRQ(ierr);
+  ierr = TaoSetHessianRoutine(tao,H,H,FormHessian,NULL); CHKERRQ(ierr);
+
+  /* Check for TAO command line options */
+  ierr = TaoSetFromOptions(tao); CHKERRQ(ierr);
+
+  /* Solve the application */
+  ierr = TaoSolve(tao); CHKERRQ(ierr);
+
+  /* Free data structures */
+  ierr = TaoDestroy(&tao); CHKERRQ(ierr);
+  ierr = VecDestroy(&x); CHKERRQ(ierr);
+  ierr = MatDestroy(&A); CHKERRQ(ierr);
+
 }
 
 double calculate_L_ijv(double* source,double* detector,double* xbox,double* ybox, double* theta){
