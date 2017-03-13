4c66acb444a5351cb149416fea3eade6db00874f
Matt Otten <mjo98@cornell.edu>
2017-03-13 12:06:27 -0500

    Added Makefile. Fixed compile bugs.

---
+++ Makefile
@@ -0,0 +1,28 @@
+CFLAGS	         =
+
+ODIR=obj
+SRCDIR=.
+
+include ${PETSC_DIR}/lib/petsc/conf/variables
+#include ${PETSC_DIR}/lib/petsc/conf/rules
+
+_DEPS =
+DEPS  = $(patsubst %,$(SRCDIR)/%,$(_DEPS))
+
+_OBJ  =
+OBJ = $(patsubst %,$(ODIR)/%,$(_OBJ))
+
+$(ODIR)/%.o: $(SRCDIR)/%.c $(DEPS)
+	@mkdir -p $(@D)
+	${PETSC_COMPILE} -c -o $@ $< $(CFLAGS) ${PETSC_KSP_LIB} ${PETSC_CC_INCLUDES}
+
+all: pjrt
+
+pjrt: $(ODIR)/pjrt.o $(OBJ)
+	-${CLINKER} -o $@ $^ $(CFLAGS) ${PETSC_KSP_LIB}
+
+.PHONY: clean
+
+clean:
+	rm -f $(OBJ)
+	rm -f pjrt
--- pjrt.c
+++ pjrt.c
@@ -1,22 +1,24 @@
 
 #include <math.h>
 #include <stdlib.h>
-//#include "petsc.h"
+#include "petsc.h"
+#include "petsctao.h"
+
+double calculate_L_ijv(double*,double*,double*,double*, double);
 
 int main(int argc,char **args){
-  int i,j,n_tau,n_theta,n_e,n_E,n_x,n_y,n_xy,e,E,v;
-  int n_total_1,n_total_2,my_start,my_end,i_p,j_p;
+  int i,j,n_tau,n_theta,n_e,n_E,n_x,n_y,n_xy,e,E,v,nid,np;
+  int n_total_1,n_total_2,my_start,my_end,i_p,j_p,n_v;
   PetscInt ierr;
-  double A_ijveE;
+  double A_ijveE, M_eE, I_ij, L_ijv,L_ijv_sum;
   double omega[4]={-2,2,-2,2};
   double primary_source[2],primary_detector[2],source[2],detector[2];
-  double *theta;
   double xbox[4]={omega[0],omega[0],omega[1],omega[1],omega[0]};
   double ybox[4]={omega[2],omega[3],omega[3],omega[2],omega[2]};
-
-
-
-  Mat A, x, MU_e;
+  double **M, *theta, *MU_e, **x_sample;
+  Tao tao;
+  Mat A;
+  Vec x;
 
   n_x=100;
   n_y=100;
@@ -45,11 +47,27 @@ int main(int argc,char **args){
   /* Get number of processors */
   ierr = MPI_Comm_size(PETSC_COMM_WORLD,&np);
 
+  // Allocate a 2D array; M is local to each core
+  M = (double **)malloc(n_e * sizeof(double *));
+  for(e=0;e<n_e;e++){
+    M[e] = (double *) malloc(n_E * sizeof(double));
+  }
+
+  x_sample = (double **)malloc(n_v * sizeof(double *));
+  for(v=0;v<n_v;v++){
+    x_sample[e] = (double *) malloc(n_e * sizeof(double));
+  }
+
+  theta = (double *) malloc(n_theta * sizeof(double));
+  theta = (double *) malloc(n_e * sizeof(double));
+
   my_start = (n_tau/np)*nid;     //0   10  20   30
   my_end   = (n_tau/np)*(nid+1); //10  20  30   40
 
   primary_source[0] = 1; primary_source[1] = 2;
   primary_detector[0] = 1; primary_detector[1] = 2;
+
+  L_ijv_sum = 0;
 
   for(i=my_start;i<my_end;i++){
     for(j=0;j<n_theta;j++){
@@ -59,28 +77,29 @@ int main(int argc,char **args){
       detector[0] = sin(theta[j])*(primary_detector[0]); //Something like this
       detector[1] = sin(theta[j])*(primary_detector[1]);
 
-      [sub_v,Lvec] = calculate_L_ijv(source,detector,xbox,ybox,theta[j]);
-      int n_subv = sizeof(sub_v);
       I_ij = 0;
-      for(v=0;v<n_subv;v++){
-        for (e=0;e<n_e;e++){
-          I_ij += x[sub_v[v],e]*Lvec[v]*MU_e[e,1];
-          // O_ijve = calculate_O(); not self-absorption for now; 
+      for(v=0;v<n_v;v++){
+        L_ijv = calculate_L_ijv(source,detector,xbox,ybox,theta[j]);
+        if (L_ijv!=0.0) {
+          for (e=0;e<n_e;e++){
+            I_ij += x_sample[v][e]*L_ijv*MU_e[e];
+            // O_ijve = calculate_O(); //no self-absorption for now
 
-          for (E=0;E<n_E;E++){
-            M_eE = M[e,E];
+            for (E=0;E<n_E;E++){
+              M_eE = M[e][E];
 
-            A_ijveE    = Lvec[v]*I_ij*M_eE;
+              A_ijveE    = L_ijv*I_ij*M_eE;
 
-            i_p        = (i-1)*n_theta+j+(E-1)*(n_theta*n_tau);
-            j_p        = (sub_v[v]-1)*n_e+e;
-            //A[i_p,j_p] = A_ijveE;
-            if (A_ijveE!=0){
-              ierr = MatSetValue(A,i_p,j_p,A_ijveE,SET_VALUES);
+              i_p        = (i-1)*n_theta+j+(E-1)*(n_theta*n_tau);
+              j_p        = (v-1)*n_e+e;
+              //A[i_p,j_p] = A_ijveE;
+              if (A_ijveE!=0){
+                ierr = MatSetValue(A,i_p,j_p,A_ijveE,INSERT_VALUES);
+              }
             }
           }
-        }
         L_ijv_sum += L_ijv_sum;
+        }
       }
     }
   }
@@ -96,8 +115,8 @@ int main(int argc,char **args){
   ierr = TaoSetInitialVector(tao,x); CHKERRQ(ierr);
 
   /* Set routines for function, gradient, etc */
-  ierr = TaoSetObjectiveAndGradientRoutine(tao,FormFunctionGradient,NULL); CHKERRQ(ierr);
-  ierr = TaoSetHessianRoutine(tao,H,H,FormHessian,NULL); CHKERRQ(ierr);
+  /* ierr = TaoSetObjectiveAndGradientRoutine(tao,FormFunctionGradient,NULL); CHKERRQ(ierr); */
+  /* ierr = TaoSetHessianRoutine(tao,H,H,FormHessian,NULL); CHKERRQ(ierr); */
 
   /* Check for TAO command line options */
   ierr = TaoSetFromOptions(tao); CHKERRQ(ierr);
@@ -110,11 +129,12 @@ int main(int argc,char **args){
   ierr = VecDestroy(&x); CHKERRQ(ierr);
   ierr = MatDestroy(&A); CHKERRQ(ierr);
 
+  return;
 }
 
-double calculate_L_ijv(double* source,double* detector,double* xbox,double* ybox, double* theta){
-
-
+double calculate_L_ijv(double* source,double* detector,double* xbox,double* ybox, double theta){
+  double L_ijv;
+  L_ijv = 1.0;
   return L_ijv;
 }
 
